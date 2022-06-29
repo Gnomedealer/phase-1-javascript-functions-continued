dde2ef51993f5f6403e954104048970778492ec8
Tommy Nguyen <sixty22tommy@gmail.com>
2022-06-28 18:42:35 -0700

    commit tree 756fea638bd7d5466dd069451fe7006debc1c78e

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(event='roller-skate'){
+    return `This Saturday, I want to ${event}!`
+}
+function mondayWork(location='go to the office'){
+    return `This Monday, I will ${location}.`
+}
+function wrapAdjective(ski='*'){
+    return function(character='special'){
+        return `You are ${ski}${character}${ski}!`
+    }
+}
+console.log(wrapAdjective()())
